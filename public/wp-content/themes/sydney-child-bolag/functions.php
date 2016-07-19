84c37078fa69579b94faa2d70f2705f4483788ee
Korede Folarin <ifolarin@webbasedsol.com>
2016-07-19 19:58:53 +0100

    service excerpts now displayed instead of content on services page

--- public/wp-content/themes/sydney-child-bolag/functions.php
+++ public/wp-content/themes/sydney-child-bolag/functions.php
@@ -16,3 +16,6 @@ add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
 require get_stylesheet_directory() . "/widgets/fp-services-type-a.php";
 require get_stylesheet_directory() . "/widgets/fp-services-type-b.php";
+
+add_filter( 'widget_text', 'shortcode_unautop');
+add_filter( 'widget_text', 'do_shortcode');
