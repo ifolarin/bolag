321da18ddd65ac1f02158f072c2d8c80d56729b2
Korede Folarin <ifolarin@webbasedsol.com>
2016-07-19 22:39:39 +0100

    completed v1.0

---
+++ public/wp-content/themes/sydney-child-bolag/footer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Sydney
+ */
+?>
+			</div>
+		</div>
+	</div><!-- #content -->
+
+	<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+		<?php get_sidebar('footer'); ?>
+	<?php endif; ?>
+
+    <a class="go-top"><i class="fa fa-angle-up"></i></a>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info container">
+			&copy; Bolag Resources Limited 2016
+			<span class="sep"> | </span>
+			<a target="_blank" href="<?php echo esc_url( __( 'https://webbasedsol.com', 'sydney' ) ); ?>"><?php printf( __( 'Powered by %s', 'sydney' ), 'Webbased Solutions' ); ?></a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
